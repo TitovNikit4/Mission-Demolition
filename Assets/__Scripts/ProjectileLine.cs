6d0c8a2ef847ba3e5a851183a004535edd8070c1
Nikita Titov <tns36reg@gmail.com>
2022-02-20 01:51:14 +0300

    Cloud movement, castle

---
+++ Assets/__Scripts/ProjectileLine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLine : MonoBehaviour {
+	static public ProjectileLine S;
+
+	[Header("Set in Inspector")]
+	public float minDist = 0.1f;
+
+	private LineRenderer line;
+	private GameObject _poi;
+	private List<Vector3> points;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/__Scripts/RigidbodySleep.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody>();
+		if (rb != null) rb.Sleep();
+	}
+}
